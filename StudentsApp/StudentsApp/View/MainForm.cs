5a166a285f6a4296b34bb50cf6db677eb5975230
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-04-23 17:55:16 +0700

    Создан новый проект для 8 лабораторной StudensApp

---
+++ StudentsApp/StudentsApp/View/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace StudentsApp.View
+{
+    /// <summary>
+    /// Содержит логику главного окна.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Создает объект типа <see cref="MainForm"/>.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
