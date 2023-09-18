402e69a6aa7a8c893a8fbcb13f5f0102cd388a11
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-09-18 21:32:13 +0700

    Закончена верстка окна Customers

--- ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomersTab.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomersTab.Designer.cs
@@ -28,18 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.EditButton = new System.Windows.Forms.Button();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.SelectedCustomerLabel = new System.Windows.Forms.Label();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
+            this.CustomersLabel = new System.Windows.Forms.Label();
+            this.FullNameLabel = new System.Windows.Forms.Label();
+            this.AddressLabel = new System.Windows.Forms.Label();
+            this.AddressTextBox = new System.Windows.Forms.TextBox();
+            this.FullNameTextBox = new System.Windows.Forms.TextBox();
+            this.Panel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // EditButton
+            // 
+            this.EditButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.EditButton.Location = new System.Drawing.Point(231, 556);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(108, 40);
+            this.EditButton.TabIndex = 21;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(407, 28);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.ReadOnly = true;
+            this.IdTextBox.Size = new System.Drawing.Size(158, 20);
+            this.IdTextBox.TabIndex = 20;
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(345, 31);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(21, 13);
+            this.IdLabel.TabIndex = 19;
+            this.IdLabel.Text = "ID:";
+            // 
+            // SelectedCustomerLabel
+            // 
+            this.SelectedCustomerLabel.AutoSize = true;
+            this.SelectedCustomerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectedCustomerLabel.Location = new System.Drawing.Point(345, 4);
+            this.SelectedCustomerLabel.Name = "SelectedCustomerLabel";
+            this.SelectedCustomerLabel.Size = new System.Drawing.Size(113, 13);
+            this.SelectedCustomerLabel.TabIndex = 18;
+            this.SelectedCustomerLabel.Text = "Selected Customer";
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RemoveButton.Location = new System.Drawing.Point(117, 556);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(108, 40);
+            this.RemoveButton.TabIndex = 17;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AddButton.Location = new System.Drawing.Point(3, 556);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(108, 40);
+            this.AddButton.TabIndex = 16;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // CustomersListBox
+            // 
+            this.CustomersListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.IntegralHeight = false;
+            this.CustomersListBox.Location = new System.Drawing.Point(3, 20);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(336, 530);
+            this.CustomersListBox.TabIndex = 15;
+            // 
+            // CustomersLabel
+            // 
+            this.CustomersLabel.AutoSize = true;
+            this.CustomersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CustomersLabel.Location = new System.Drawing.Point(3, 4);
+            this.CustomersLabel.Name = "CustomersLabel";
+            this.CustomersLabel.Size = new System.Drawing.Size(65, 13);
+            this.CustomersLabel.TabIndex = 14;
+            this.CustomersLabel.Text = "Customers";
+            // 
+            // FullNameLabel
+            // 
+            this.FullNameLabel.AutoSize = true;
+            this.FullNameLabel.Location = new System.Drawing.Point(345, 60);
+            this.FullNameLabel.Name = "FullNameLabel";
+            this.FullNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.FullNameLabel.TabIndex = 22;
+            this.FullNameLabel.Text = "Full Name:";
+            // 
+            // AddressLabel
+            // 
+            this.AddressLabel.AutoSize = true;
+            this.AddressLabel.Location = new System.Drawing.Point(345, 89);
+            this.AddressLabel.Name = "AddressLabel";
+            this.AddressLabel.Size = new System.Drawing.Size(48, 13);
+            this.AddressLabel.TabIndex = 23;
+            this.AddressLabel.Text = "Address:";
+            // 
+            // AddressTextBox
+            // 
+            this.AddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddressTextBox.Location = new System.Drawing.Point(407, 86);
+            this.AddressTextBox.Multiline = true;
+            this.AddressTextBox.Name = "AddressTextBox";
+            this.AddressTextBox.ReadOnly = true;
+            this.AddressTextBox.Size = new System.Drawing.Size(790, 94);
+            this.AddressTextBox.TabIndex = 24;
+            // 
+            // FullNameTextBox
+            // 
+            this.FullNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FullNameTextBox.Location = new System.Drawing.Point(407, 57);
+            this.FullNameTextBox.Name = "FullNameTextBox";
+            this.FullNameTextBox.ReadOnly = true;
+            this.FullNameTextBox.Size = new System.Drawing.Size(790, 20);
+            this.FullNameTextBox.TabIndex = 25;
+            // 
+            // Panel
+            // 
+            this.Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel.Location = new System.Drawing.Point(348, 186);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(849, 410);
+            this.Panel.TabIndex = 26;
             // 
             // CustomersTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.FullNameTextBox);
+            this.Controls.Add(this.AddressTextBox);
+            this.Controls.Add(this.AddressLabel);
+            this.Controls.Add(this.FullNameLabel);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.IdTextBox);
+            this.Controls.Add(this.IdLabel);
+            this.Controls.Add(this.SelectedCustomerLabel);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.CustomersListBox);
+            this.Controls.Add(this.CustomersLabel);
             this.Name = "CustomersTab";
-            this.Size = new System.Drawing.Size(822, 645);
+            this.Size = new System.Drawing.Size(1200, 599);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.Label SelectedCustomerLabel;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.ListBox CustomersListBox;
+        private System.Windows.Forms.Label CustomersLabel;
+        private System.Windows.Forms.Label FullNameLabel;
+        private System.Windows.Forms.Label AddressLabel;
+        private System.Windows.Forms.TextBox AddressTextBox;
+        private System.Windows.Forms.TextBox FullNameTextBox;
+        private System.Windows.Forms.Panel Panel;
     }
 }
