fdac3c85d7ba333f8059ad9c53b1efc439ea29fd
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-10-01 19:13:49 +0700

    Создан класс Address и закончена верстка элемента управления AddressControl

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Address.cs
@@ -0,0 +1,149 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные об адресе.
+    /// </summary>
+    public class Address
+    {
+        /// <summary>
+        /// Почтовый индекс.
+        /// </summary>
+        private int _index;
+
+        /// <summary>
+        /// Страна/регион.
+        /// </summary>
+        private string _country;
+
+        /// <summary>
+        /// Город(населенный пункт).
+        /// </summary>
+        private string _city;
+
+        /// <summary>
+        /// Улица.
+        /// </summary>
+        private string _street;
+
+        /// <summary>
+        /// Номер дома.
+        /// </summary>
+        private string _building;
+
+        /// <summary>
+        /// Номер квартиры/помещения.
+        /// </summary>
+        private string _apartment;
+
+        /// <summary>
+        /// Возвращает и задает почтовый индекс. Должен состоять из 6 цифр.
+        /// </summary>
+        public int Index
+        {
+            get => _index;
+            set
+            {
+                if (value.ToString().Length != 6)
+                {
+                    throw new ArgumentException("Индекс должен состоять из 6 цифр.");
+                }
+                _index = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает страну/регион. Не должна быть длиннее 50 символов.
+        /// </summary>
+        public string Country
+        {
+            get => _country;
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50);
+                _country = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает или задает город(населенный пункт). Не должен быть длиннее 50 символов.
+        /// </summary>
+        public string City
+        {
+            get => _city;
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50);
+                _city = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает или задает улицу. Не должна быть длиннее 100 символов.
+        /// </summary>
+        public string Street
+        {
+            get => _street;
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 100);
+                _street = value;
+            }
+        }
+        
+        /// <summary>
+        /// Врзвращает или задает номер дома. Не должен быть длиннее 10 символов.
+        /// </summary>
+        public string Building
+        {
+            get => _building;
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10);
+                _building = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает или задает номер квартиры/помещения. Не должен быть длиннее 10 символов.
+        /// </summary>
+        public string Apartment
+        {
+            get => _apartment;
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10);
+                _apartment = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Address"/> без его инициализации.
+        /// </summary>
+        public Address()
+        {
+
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Address"/>.
+        /// </summary>
+        /// <param name="index">Почтовый индекс. Должен состоять из 6 цифр.</param>
+        /// <param name="country">Страна/регион. Не должна быть длиннее 50 символов.</param>
+        /// <param name="city">Город(населенный пункт). Не должен быть длиннее 50 символов.</param>
+        /// <param name="street">Улица. Не должна быть длиннее 100 символов.</param>
+        /// <param name="building">Номер дома. Не должен быть длиннее 10 символов.</param>
+        /// <param name="apartment">Номер квартиры/помещения. Не должен быть длиннее 10 символов.</param>
+        public Address(int index, string country, string city, string street, string building, string apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            Building = building;
+            Apartment = apartment;
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ObjectOrientedPractics.View.Controls
+{
+    partial class AddressControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DeliveryAddressLabel = new System.Windows.Forms.Label();
+            this.IndexLabel = new System.Windows.Forms.Label();
+            this.CounrtyLabel = new System.Windows.Forms.Label();
+            this.StreetLabel = new System.Windows.Forms.Label();
+            this.BuildingLabel = new System.Windows.Forms.Label();
+            this.ApartmentLabel = new System.Windows.Forms.Label();
+            this.CityLabel = new System.Windows.Forms.Label();
+            this.IndexTextBox = new System.Windows.Forms.TextBox();
+            this.CountryTextBox = new System.Windows.Forms.TextBox();
+            this.StreetTextBox = new System.Windows.Forms.TextBox();
+            this.BuildingTextBox = new System.Windows.Forms.TextBox();
+            this.ApartmentTextBox = new System.Windows.Forms.TextBox();
+            this.CityTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DeliveryAddressLabel
+            // 
+            this.DeliveryAddressLabel.AutoSize = true;
+            this.DeliveryAddressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeliveryAddressLabel.Location = new System.Drawing.Point(3, 0);
+            this.DeliveryAddressLabel.Name = "DeliveryAddressLabel";
+            this.DeliveryAddressLabel.Size = new System.Drawing.Size(102, 13);
+            this.DeliveryAddressLabel.TabIndex = 0;
+            this.DeliveryAddressLabel.Text = "Delivery Address";
+            // 
+            // IndexLabel
+            // 
+            this.IndexLabel.AutoSize = true;
+            this.IndexLabel.Location = new System.Drawing.Point(3, 19);
+            this.IndexLabel.Name = "IndexLabel";
+            this.IndexLabel.Size = new System.Drawing.Size(60, 13);
+            this.IndexLabel.TabIndex = 1;
+            this.IndexLabel.Text = "Post Index:";
+            // 
+            // CounrtyLabel
+            // 
+            this.CounrtyLabel.AutoSize = true;
+            this.CounrtyLabel.Location = new System.Drawing.Point(3, 45);
+            this.CounrtyLabel.Name = "CounrtyLabel";
+            this.CounrtyLabel.Size = new System.Drawing.Size(46, 13);
+            this.CounrtyLabel.TabIndex = 2;
+            this.CounrtyLabel.Text = "Counrty:";
+            // 
+            // StreetLabel
+            // 
+            this.StreetLabel.AutoSize = true;
+            this.StreetLabel.Location = new System.Drawing.Point(3, 73);
+            this.StreetLabel.Name = "StreetLabel";
+            this.StreetLabel.Size = new System.Drawing.Size(38, 13);
+            this.StreetLabel.TabIndex = 3;
+            this.StreetLabel.Text = "Street:";
+            // 
+            // BuildingLabel
+            // 
+            this.BuildingLabel.AutoSize = true;
+            this.BuildingLabel.Location = new System.Drawing.Point(3, 99);
+            this.BuildingLabel.Name = "BuildingLabel";
+            this.BuildingLabel.Size = new System.Drawing.Size(47, 13);
+            this.BuildingLabel.TabIndex = 4;
+            this.BuildingLabel.Text = "Building:";
+            // 
+            // ApartmentLabel
+            // 
+            this.ApartmentLabel.AutoSize = true;
+            this.ApartmentLabel.Location = new System.Drawing.Point(175, 99);
+            this.ApartmentLabel.Name = "ApartmentLabel";
+            this.ApartmentLabel.Size = new System.Drawing.Size(58, 13);
+            this.ApartmentLabel.TabIndex = 5;
+            this.ApartmentLabel.Text = "Apartment:";
+            // 
+            // CityLabel
+            // 
+            this.CityLabel.AutoSize = true;
+            this.CityLabel.Location = new System.Drawing.Point(289, 45);
+            this.CityLabel.Name = "CityLabel";
+            this.CityLabel.Size = new System.Drawing.Size(27, 13);
+            this.CityLabel.TabIndex = 6;
+            this.CityLabel.Text = "City:";
+            // 
+            // IndexTextBox
+            // 
+            this.IndexTextBox.Location = new System.Drawing.Point(69, 16);
+            this.IndexTextBox.Name = "IndexTextBox";
+            this.IndexTextBox.Size = new System.Drawing.Size(121, 20);
+            this.IndexTextBox.TabIndex = 7;
+            // 
+            // CountryTextBox
+            // 
+            this.CountryTextBox.Location = new System.Drawing.Point(69, 42);
+            this.CountryTextBox.Name = "CountryTextBox";
+            this.CountryTextBox.Size = new System.Drawing.Size(214, 20);
+            this.CountryTextBox.TabIndex = 8;
+            // 
+            // StreetTextBox
+            // 
+            this.StreetTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StreetTextBox.Location = new System.Drawing.Point(69, 70);
+            this.StreetTextBox.Name = "StreetTextBox";
+            this.StreetTextBox.Size = new System.Drawing.Size(499, 20);
+            this.StreetTextBox.TabIndex = 9;
+            // 
+            // BuildingTextBox
+            // 
+            this.BuildingTextBox.Location = new System.Drawing.Point(69, 96);
+            this.BuildingTextBox.Name = "BuildingTextBox";
+            this.BuildingTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BuildingTextBox.TabIndex = 10;
+            // 
+            // ApartmentTextBox
+            // 
+            this.ApartmentTextBox.Location = new System.Drawing.Point(239, 96);
+            this.ApartmentTextBox.Name = "ApartmentTextBox";
+            this.ApartmentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ApartmentTextBox.TabIndex = 11;
+            // 
+            // CityTextBox
+            // 
+            this.CityTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CityTextBox.Location = new System.Drawing.Point(322, 42);
+            this.CityTextBox.Name = "CityTextBox";
+            this.CityTextBox.Size = new System.Drawing.Size(246, 20);
+            this.CityTextBox.TabIndex = 12;
+            // 
+            // AddressControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CityTextBox);
+            this.Controls.Add(this.ApartmentTextBox);
+            this.Controls.Add(this.BuildingTextBox);
+            this.Controls.Add(this.StreetTextBox);
+            this.Controls.Add(this.CountryTextBox);
+            this.Controls.Add(this.IndexTextBox);
+            this.Controls.Add(this.CityLabel);
+            this.Controls.Add(this.ApartmentLabel);
+            this.Controls.Add(this.BuildingLabel);
+            this.Controls.Add(this.StreetLabel);
+            this.Controls.Add(this.CounrtyLabel);
+            this.Controls.Add(this.IndexLabel);
+            this.Controls.Add(this.DeliveryAddressLabel);
+            this.Name = "AddressControl";
+            this.Size = new System.Drawing.Size(571, 123);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DeliveryAddressLabel;
+        private System.Windows.Forms.Label IndexLabel;
+        private System.Windows.Forms.Label CounrtyLabel;
+        private System.Windows.Forms.Label StreetLabel;
+        private System.Windows.Forms.Label BuildingLabel;
+        private System.Windows.Forms.Label ApartmentLabel;
+        private System.Windows.Forms.Label CityLabel;
+        private System.Windows.Forms.TextBox IndexTextBox;
+        private System.Windows.Forms.TextBox CountryTextBox;
+        private System.Windows.Forms.TextBox StreetTextBox;
+        private System.Windows.Forms.TextBox BuildingTextBox;
+        private System.Windows.Forms.TextBox ApartmentTextBox;
+        private System.Windows.Forms.TextBox CityTextBox;
+    }
+}
