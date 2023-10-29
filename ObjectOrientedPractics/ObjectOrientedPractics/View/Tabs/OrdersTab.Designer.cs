986a9d1fed5121d83add906242ed9884fe07fc54
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-10-29 14:39:20 +0700

    Завершена верстка OrderTab

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/OrdersTab.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class OrdersTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OrdersLabel = new System.Windows.Forms.Label();
+            this.OrdersDataGridView = new System.Windows.Forms.DataGridView();
+            this.SelectedOrderLabel = new System.Windows.Forms.Label();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.DataLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.OrderItemsLabel = new System.Windows.Forms.Label();
+            this.AmountLabel = new System.Windows.Forms.Label();
+            this.TotalAmountLabel = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.DataTextBox = new System.Windows.Forms.TextBox();
+            this.StatusComboBox = new System.Windows.Forms.ComboBox();
+            this.OrderItemsListBox = new System.Windows.Forms.ListBox();
+            this.AddressControl = new ObjectOrientedPractics.View.Controls.AddressControl();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OrdersLabel
+            // 
+            this.OrdersLabel.AutoSize = true;
+            this.OrdersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OrdersLabel.Location = new System.Drawing.Point(3, 4);
+            this.OrdersLabel.Name = "OrdersLabel";
+            this.OrdersLabel.Size = new System.Drawing.Size(44, 13);
+            this.OrdersLabel.TabIndex = 0;
+            this.OrdersLabel.Text = "Orders";
+            // 
+            // OrdersDataGridView
+            // 
+            this.OrdersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrdersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersDataGridView.Location = new System.Drawing.Point(3, 20);
+            this.OrdersDataGridView.Name = "OrdersDataGridView";
+            this.OrdersDataGridView.Size = new System.Drawing.Size(500, 658);
+            this.OrdersDataGridView.TabIndex = 1;
+            // 
+            // SelectedOrderLabel
+            // 
+            this.SelectedOrderLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectedOrderLabel.AutoSize = true;
+            this.SelectedOrderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectedOrderLabel.Location = new System.Drawing.Point(509, 4);
+            this.SelectedOrderLabel.Name = "SelectedOrderLabel";
+            this.SelectedOrderLabel.Size = new System.Drawing.Size(88, 13);
+            this.SelectedOrderLabel.TabIndex = 2;
+            this.SelectedOrderLabel.Text = "SelectedOrder";
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(509, 29);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(21, 13);
+            this.IdLabel.TabIndex = 3;
+            this.IdLabel.Text = "ID:";
+            // 
+            // DataLabel
+            // 
+            this.DataLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataLabel.AutoSize = true;
+            this.DataLabel.Location = new System.Drawing.Point(509, 55);
+            this.DataLabel.Name = "DataLabel";
+            this.DataLabel.Size = new System.Drawing.Size(47, 13);
+            this.DataLabel.TabIndex = 4;
+            this.DataLabel.Text = "Created:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(509, 81);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(40, 13);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Status:";
+            // 
+            // OrderItemsLabel
+            // 
+            this.OrderItemsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrderItemsLabel.AutoSize = true;
+            this.OrderItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OrderItemsLabel.Location = new System.Drawing.Point(509, 249);
+            this.OrderItemsLabel.Name = "OrderItemsLabel";
+            this.OrderItemsLabel.Size = new System.Drawing.Size(72, 13);
+            this.OrderItemsLabel.TabIndex = 6;
+            this.OrderItemsLabel.Text = "Order Items";
+            // 
+            // AmountLabel
+            // 
+            this.AmountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AmountLabel.AutoSize = true;
+            this.AmountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AmountLabel.Location = new System.Drawing.Point(1037, 476);
+            this.AmountLabel.Name = "AmountLabel";
+            this.AmountLabel.Size = new System.Drawing.Size(53, 13);
+            this.AmountLabel.TabIndex = 7;
+            this.AmountLabel.Text = "Amount:";
+            // 
+            // TotalAmountLabel
+            // 
+            this.TotalAmountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.TotalAmountLabel.AutoSize = true;
+            this.TotalAmountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TotalAmountLabel.Location = new System.Drawing.Point(960, 489);
+            this.TotalAmountLabel.Name = "TotalAmountLabel";
+            this.TotalAmountLabel.Size = new System.Drawing.Size(25, 25);
+            this.TotalAmountLabel.TabIndex = 8;
+            this.TotalAmountLabel.Text = "0";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.IdTextBox.Location = new System.Drawing.Point(575, 26);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(121, 20);
+            this.IdTextBox.TabIndex = 9;
+            // 
+            // DataTextBox
+            // 
+            this.DataTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataTextBox.Location = new System.Drawing.Point(575, 52);
+            this.DataTextBox.Name = "DataTextBox";
+            this.DataTextBox.Size = new System.Drawing.Size(121, 20);
+            this.DataTextBox.TabIndex = 10;
+            // 
+            // StatusComboBox
+            // 
+            this.StatusComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusComboBox.FormattingEnabled = true;
+            this.StatusComboBox.Location = new System.Drawing.Point(575, 78);
+            this.StatusComboBox.Name = "StatusComboBox";
+            this.StatusComboBox.Size = new System.Drawing.Size(121, 21);
+            this.StatusComboBox.TabIndex = 11;
+            // 
+            // OrderItemsListBox
+            // 
+            this.OrderItemsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrderItemsListBox.FormattingEnabled = true;
+            this.OrderItemsListBox.IntegralHeight = false;
+            this.OrderItemsListBox.Location = new System.Drawing.Point(512, 265);
+            this.OrderItemsListBox.Name = "OrderItemsListBox";
+            this.OrderItemsListBox.Size = new System.Drawing.Size(578, 208);
+            this.OrderItemsListBox.TabIndex = 12;
+            // 
+            // AddressControl
+            // 
+            this.AddressControl.Address = null;
+            this.AddressControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddressControl.Location = new System.Drawing.Point(506, 114);
+            this.AddressControl.Name = "AddressControl";
+            this.AddressControl.Size = new System.Drawing.Size(584, 123);
+            this.AddressControl.TabIndex = 13;
+            // 
+            // OrdersTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.AddressControl);
+            this.Controls.Add(this.OrderItemsListBox);
+            this.Controls.Add(this.StatusComboBox);
+            this.Controls.Add(this.DataTextBox);
+            this.Controls.Add(this.IdTextBox);
+            this.Controls.Add(this.TotalAmountLabel);
+            this.Controls.Add(this.AmountLabel);
+            this.Controls.Add(this.OrderItemsLabel);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.DataLabel);
+            this.Controls.Add(this.IdLabel);
+            this.Controls.Add(this.SelectedOrderLabel);
+            this.Controls.Add(this.OrdersDataGridView);
+            this.Controls.Add(this.OrdersLabel);
+            this.Name = "OrdersTab";
+            this.Size = new System.Drawing.Size(1093, 681);
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OrdersLabel;
+        private System.Windows.Forms.DataGridView OrdersDataGridView;
+        private System.Windows.Forms.Label SelectedOrderLabel;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.Label DataLabel;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label OrderItemsLabel;
+        private System.Windows.Forms.Label AmountLabel;
+        private System.Windows.Forms.Label TotalAmountLabel;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.TextBox DataTextBox;
+        private System.Windows.Forms.ComboBox StatusComboBox;
+        private System.Windows.Forms.ListBox OrderItemsListBox;
+        private Controls.AddressControl AddressControl;
+    }
+}
