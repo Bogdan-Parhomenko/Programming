71dc8415d075457299cd5d1a898c56e56766f954
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-11-07 18:25:48 +0700

    Добавлен класс PriorityOrder

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PriorityOrder : Order
+    {
+        public DateTime DesiredDeliveryDate { get; set; }
+
+        public string  DesiredDeliveryTime { get; set; }
+
+        public PriorityOrder()
+        {
+
+        }
+
+        public PriorityOrder(DateTime desiredDeliveryDate, string desiredDeliveryTime)
+        {
+            DesiredDeliveryDate = desiredDeliveryDate;
+            DesiredDeliveryTime = desiredDeliveryTime;
+        }
+    }
+}
