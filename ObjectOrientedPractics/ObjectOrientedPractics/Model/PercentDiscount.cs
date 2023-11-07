89a4ce5a837ff26fc0d53adf0045e9b937a8bcdd
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2023-11-07 22:03:23 +0700

    Добавлены классы скидок

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PercentDiscount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ObjectOrientedPractics.Model
+{
+    public class PercentDiscount
+    {
+        private int _accumulatedPercents;
+
+        public Category Category { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public int AccumulatedPercents
+        {
+            get => _accumulatedPercents;
+            private set
+            {
+                if (value < 1 || value > 10)
+                {
+                    throw new ArgumentException("Количество накопленных процентов не божеть быть отрицательным или больше 10.");
+                }
+                _accumulatedPercents = value;
+            }
+        }
+
+        public double Calculate(List<Item> items)
+        {
+            var totalCost = 0.0;
+            foreach (var item in items)
+            {
+                if (item.Category == Category)
+                {
+                    totalCost += item.Cost;
+                }
+            }
+            return totalCost * (AccumulatedPercents / 100);
+        }
+
+        public double Apply(List<Item> items)
+        {
+            var discount = Calculate(items);
+            foreach (var item in items)
+            {
+                if (item.Category == Category)
+                {
+                    item.Cost -= discount / items.Count;
+                }
+            }
+            return (int)discount;
+        }
+
+        public void Update(List<Item> items)
+        {
+            var totalCost = 0.0;
+            foreach (var item in items)
+            {
+                if (item.Category == Category)
+                {
+                    totalCost += item.Cost;
+                }
+            }
+            if (totalCost >= 1000)
+            {
+                AccumulatedPercents += 1;
+            }
+        }
+
+        public string Info()
+        {
+            return($"Процентная «{Category}» - {AccumulatedPercents}%");
+        }
+
+        public PercentDiscount()
+        {
+            AccumulatedPercents = 1;
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PointsDiscount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PointsDiscount
+    {
+        private int _accumulatedPoints;
+
+        public int AccumulatedPoints
+        {
+            get => _accumulatedPoints;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Количество накопленных баллов не божеть быть отрицательным.");
+                }
+                _accumulatedPoints = value;
+            }
+        }
+
+        public double Calculate(List<Item> items)
+        {
+            var totalCost = 0.0;
+            foreach (var item in items)
+            {
+                totalCost += item.Cost;
+            }
+            totalCost *= 0.3;
+            if (AccumulatedPoints >= (int)totalCost)
+            {
+                return (int)totalCost;
+            }
+            else
+            {
+                return AccumulatedPoints;
+            }
+        }
+
+        public double Apply(List<Item> items)
+        {
+            var discount = Calculate(items);
+            foreach (var item in items)
+            {
+                item.Cost -= discount / items.Count;
+            }
+            AccumulatedPoints -= (int)discount;
+            return (int)discount;
+        }
+
+        public void Update(List<Item> items)
+        {
+            var totalCost = 0.0;
+            foreach (var item in items)
+            {
+                totalCost += item.Cost;
+            }
+            AccumulatedPoints += (int)(totalCost * 0.1);
+        }
+
+        public string Info()
+        {
+            return $"Накопительная – {AccumulatedPoints} баллов";
+        }
+
+        public PointsDiscount()
+        {
+
+        }
+    }
+}
