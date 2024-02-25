276883f33df60c0b0e2f03be27d4bfd82b9711a3
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2024-02-25 20:00:20 +0700

    Добавлена привязка данных

---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private Contact _contact;
+
+        public string Name
+        {
+            get
+            {
+                return _contact.Name;
+            }
+            set
+            {
+                _contact.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return _contact.Phone;
+            }
+            set
+            {
+                _contact.Phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _contact.Email;
+            }
+            set
+            {
+                _contact.Email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public MainVM()
+        {
+            _contact = new Contact { Name = "Bob", Phone = "89", Email = "@" };
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
