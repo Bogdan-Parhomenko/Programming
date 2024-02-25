316f0767e67a81d4b348ed799a38226837f56c2f
Богдан Пархоменко <parhomenkobogdan1111@gmail.com>
2024-02-25 18:04:42 +0700

    частичная верстка Contacts

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,55 @@
+﻿namespace View.Model
+{
+    public class Contact
+    {
+        private string _name;
+        private string _phone;
+        private string _email;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                _phone = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
